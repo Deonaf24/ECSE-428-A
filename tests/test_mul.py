2f436e343bd808118d862347ce77c4277cd0d0bb
JeremiasZimmeramn213 <jeremias2205@outlook.com>
2026-02-09 14:15:41 -0500

    T-MUL-CPLX1 add test for multiplying two complex numbers

--- tests/test_mul.py
+++ tests/test_mul.py
@@ -12,3 +12,12 @@ def test_mul_real1():
     result = engine.execute("pop")
     assert result == "-6 + j0"
 
+#T-MUL-CPLX1
+def test_mul_cplx1():
+    """T-MUL-CPLX1: Multiply two complex numbers (1+j2) * (3-j4) = 11+j2"""
+    engine = Engine()
+    engine.execute("push 1+j2")
+    engine.execute("push 3-j4")
+    engine.execute("mul")
+    result = engine.execute("pop")
+    assert result == "11 + j2"
