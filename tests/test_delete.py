d17743d47f05faa39fda8067e8e7681898f71925
JeremiasZimmeramn213 <jeremias2205@outlook.com>
2026-02-09 15:10:58 -0500

    add test for deleting top of stack with complex numbers

--- tests/test_delete.py
+++ tests/test_delete.py
@@ -10,3 +10,13 @@ def test_del_real1():
     engine.execute("delete")
     result = engine.execute("pop")
     assert result == "1 + j0"
+
+# T-DEL-CPLX1
+def test_del_cplx1():
+    """T-DEL-CPLX1: Delete top of stack with complex numbers"""
+    engine = Engine()
+    engine.execute("push 1+j1")
+    engine.execute("push 2+j3")
+    engine.execute("delete")
+    result = engine.execute("pop")
+    assert result == "1 + j1"
