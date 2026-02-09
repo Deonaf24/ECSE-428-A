453413c4ade6f6f539a83eaf627cc99f05617aff
JeremiasZimmeramn213 <jeremias2205@outlook.com>
2026-02-09 14:56:17 -0500

    add test for division of two complex numbers

--- tests/test_div.py
+++ tests/test_div.py
@@ -11,3 +11,13 @@ def test_div_real1():
     engine.execute("div")
     result = engine.execute("pop")
     assert result == "4 + j0"
+
+# T-DIV-CPLX1
+def test_div_cplx1():
+    """T-DIV-CPLX1: Divide two complex numbers (4+j2) / (1+j1) = 3-j1"""
+    engine = Engine()
+    engine.execute("push 4+j2")
+    engine.execute("push 1+j1")
+    engine.execute("div")
+    result = engine.execute("pop")
+    assert result == "3 - j1"
