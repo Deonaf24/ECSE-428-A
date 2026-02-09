319edb36cac6974e51c2ba856988b5dbd5550ac3
Deonaf24 <deonaftahi@gmail.com>
2026-02-09 14:55:03 -0500

    T-SUB-CPLX1

--- tests/test_sub.py
+++ tests/test_sub.py
@@ -7,3 +7,11 @@ def test_sub_real_then_pop():
     c.execute("push 2")
     c.execute("sub")
     assert c.execute("pop") == "3 + j0"
+
+# T-SUB-CPLX1
+def test_sub_complex_then_pop():
+    c = Engine()
+    c.execute("push 3+j4")
+    c.execute("push 1-j2")
+    c.execute("sub")
+    assert c.execute("pop") == "2 + j6"
